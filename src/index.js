78c93cb586cf922840aba9d27d4926d3200be8cc
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-04-19 20:20:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
